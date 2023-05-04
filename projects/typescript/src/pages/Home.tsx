d070baa5cde1e2312440eeb8a195730253761fd4
JeonghoPark <pjhpjh0805@naver.com>
2023-05-04 23:13:47 +0900

    styled-component적용

---
+++ projects/typescript/src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import StyledButton from '../style/StyledButton';
+
+
+
+const Home = () => {
+
+   
+
+    return (
+        <div>
+            <h1>홈화면</h1>
+            <StyledButton>click</StyledButton>
+        </div>
+    );
+};
+
+export default Home;
